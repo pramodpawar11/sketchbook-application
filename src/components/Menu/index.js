f5006d501b441bad05d40aa23ec1ce42662c6279
pramod pawar <kingpramodpawar511@gmail.com>
2024-05-15 16:35:28 +0530

    Created the Menu Ui

---
+++ src/components/Menu/index.js
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import {
+  faPencil,
+  faEraser,
+  faRotateLeft,
+  faRotateRight,
+  faFileArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./index.module.css";
+
+function Index() {
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
+      </div>
+    </div>
+  );
+}
+export default Index;
