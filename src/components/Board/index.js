3a05e2346a224c12b9ff6c8314c53a70e3282f31
pramod pawar <kingpramodpawar511@gmail.com>
2024-05-23 21:43:17 +0530

    Add a ability to UNDO and REDO

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,30 +3,41 @@ import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
 
-
 const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
+  const drawHistory = useRef([]);
+  const historyPointer = useRef(0);
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
-  const actionMenuItem = useSelector((state)=>state.menu.actionMenuItem);
+  const actionMenuItem = useSelector((state) => state.menu.actionMenuItem);
   const { size, color } = useSelector((state) => state.toolbox[activeMenuItem]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    if(actionMenuItem === MENU_ITEMS.DOWNLOAD){
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
       const URL = canvas.toDataURL();
-      const ancor = document.createElement('a');
-      ancor.href = URL
-      ancor.download = "sketch.jpg"
-      ancor.click()
+      const ancor = document.createElement("a");
+      ancor.href = URL;
+      ancor.download = "sketch.jpg";
+      ancor.click();
+    }else if(actionMenuItem===MENU_ITEMS.UNDO || actionMenuItem===MENU_ITEMS.REDO){
+      if(historyPointer.current>0 && actionMenuItem === MENU_ITEMS.UNDO){
+        historyPointer.current-=1;
+      }
+      if(historyPointer.current<drawHistory.current.length-1 && actionMenuItem === MENU_ITEMS.REDO){
+        historyPointer.current+=1;
+      }
+      const imageData = drawHistory.current[historyPointer.current];
+      context.putImageData(imageData,0,0);
+      
+
     }
     dispatch(actionItemClick(null));
-  },[actionMenuItem])
+  }, [actionMenuItem]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -54,17 +65,20 @@ const Board = () => {
     const handlemousedown = (e) => {
       beginPath(e.clientX, e.clientY);
     };
-    const drawLine = (x,y)=>{
+    const drawLine = (x, y) => {
       if (!shouldDraw.current) return;
-      context.lineTo(x,y);
+      context.lineTo(x, y);
       context.stroke();
-
-    }
+    };
     const handlemousemove = (e) => {
       drawLine(e.clientX, e.clientY);
     };
     const handlemouseup = (e) => {
       shouldDraw.current = false;
+      const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length-1;
+
     };
 
     canvas.addEventListener("mousedown", handlemousedown);
